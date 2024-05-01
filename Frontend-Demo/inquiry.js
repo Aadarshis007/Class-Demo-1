c62a8915389592838b87eba8b753a91b1d17bde8
Aadarshis007 <105623094+Aadarshis007@users.noreply.github.com>
2024-05-01 12:16:11 +0530

    updated code

---
+++ Frontend-Demo/inquiry.js
@@ -0,0 +1,16 @@
+function formDetails () {
+    fullName = document.getElementById("fullName").value
+    email = document.getElementById("email").value
+    phone = document.getElementById("phone").value
+    education = document.getElementById("education").value
+    course = document.getElementById("courseInterested").value
+    sub = document.getElementById("subscribe").value
+
+    console.log("===>form details are below<===");
+    console.log("Full Name", fullName);
+    console.log("Email", email);
+    console.log("Phone", phone);
+    console.log("Education", education);
+    console.log("Course", course);
+    console.log("Subscribe", sub);
+}
