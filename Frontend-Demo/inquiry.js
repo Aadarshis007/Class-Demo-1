4e6b1d6f482035384bdba7bad427fcfb02818932
Aadarshis007 <105623094+Aadarshis007@users.noreply.github.com>
2024-05-02 12:07:22 +0530

    updated code

--- Frontend-Demo/inquiry.js
+++ Frontend-Demo/inquiry.js
@@ -1,16 +1,69 @@
-function formDetails () {
-    fullName = document.getElementById("fullName").value
-    email = document.getElementById("email").value
-    phone = document.getElementById("phone").value
-    education = document.getElementById("education").value
-    course = document.getElementById("courseInterested").value
-    sub = document.getElementById("subscribe").value
+// document.getElementById("myForm").addEventListener("submit", function (e) {
+//   e.preventDefault();
 
-    console.log("===>form details are below<===");
-    console.log("Full Name", fullName);
-    console.log("Email", email);
-    console.log("Phone", phone);
-    console.log("Education", education);
-    console.log("Course", course);
-    console.log("Subscribe", sub);
+//   // Get form values
+//   fullName = document.getElementById("fullName").value;
+//   email = document.getElementById("email").value;
+//   phone = document.getElementById("phone").value;
+//   education = document.getElementById("education").value;
+//   course = document.getElementById("courseInterested").value;
+//   sub = document.getElementById("subscribe").value;
+
+//   // Create object to hold form data
+//   var formData = {
+//     fullName: fullName,
+//     email: email,
+//     phone: phone,
+//     education: education,
+//     course: course,
+//     sub: sub,
+//   };
+
+//   // Convert object to JSON string
+//   var formDataJSON = JSON.stringify(formData);
+
+//   // Save form data to local storage
+//   localStorage.setItem("formData", formDataJSON);
+
+//   // Reset form
+//   this.reset();
+
+//   alert("Form data saved successfully!");
+// });
+
+document.getElementById("myForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  fullName = document.getElementById("fullName").value;
+  email = document.getElementById("email").value;
+  phone = document.getElementById("phone").value;
+  education = document.getElementById("education").value;
+  course = document.getElementById("courseInterested").value;
+  sub = document.getElementById("subscribe").value;
+
+  data= {
+    name : fullName,
+    mail : email,
+    phone : phone,
+    education : education,
+    course : course,
+    subscribe : sub
+  }
+
+  var jsonstring = JSON.stringify(data);
+
+  localStorage.setItem("data", jsonstring)
+
+  this.reset();
+
+  console.log("your data is submitted.");
+});
+
+function popupMessage(event) {
+    if (event == 'close') {
+        document.getElementById("successPopup").style.display = 'none';
+    }
+    else {
+        document.getElementById("successPopup").style.display = 'flex';
+    }
 }
